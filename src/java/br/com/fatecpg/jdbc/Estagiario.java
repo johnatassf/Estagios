33f225045694d1b86133acd7b4959aa091a23451
Rafael Teixeira Miguel <teixeirarafael12@gmail.com>
2019-12-01 22:45:08 -0300

    CRUD simples de estagiário

---
+++ src/java/br/com/fatecpg/jdbc/Estagiario.java
@@ -0,0 +1,66 @@
+package br.com.fatecpg.jdbc;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Rafael Teixeira Miguel
+ */
+public class Estagiario {
+    private int id;
+    private String nome;
+    private Curso curso;
+    private Setor setor;
+    private Date dataInicio;
+    private Date dataFim;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public Setor getSetor() {
+        return setor;
+    }
+
+    public void setSetor(Setor setor) {
+        this.setor = setor;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+    
+    
+}
---
+++ src/java/br/com/fatecpg/jdbc/Setor.java
@@ -0,0 +1,19 @@
+package br.com.fatecpg.jdbc;
+
+/**
+ *
+ * @author Rafael Teixeira Miguel
+ */
+public class Setor {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
